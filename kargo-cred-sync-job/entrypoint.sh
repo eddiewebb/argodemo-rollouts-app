fbe94d27fa9ec765eca16d082f7ed0abd027fc2d
Eddie Webbinaro <5262154+eddiewebb@users.noreply.github.com>
2026-01-05 15:07:35 -0500

    kargo sync job: fix docker image to run script

--- kargo-cred-sync-job/entrypoint.sh
+++ kargo-cred-sync-job/entrypoint.sh
@@ -14,28 +14,28 @@ image_url=$(kargo get warehouses -p $project -o json | jq -r '.items[]?.spec // 
 let i=0
 for repo_url in $repo_urls; do
     echo -e "\tUsing repo URL: $repo_url"
-    #echo -e "\tUsing GH PAT var name: $gh_pat_var_name"
+    #echo -e "\tUsing GH PAT var name: $gITHUB_pat_var_name"
     # Publish git credentials to Kargo secrets
     kargo create credentials github-creds${i} \
     --project $project --git \
-    --username ${GITHUB_USER} --password ${GH_PAT} \
+    --username ${GITHUB_USER} --password ${GITHUB_PAT} \
     --repo-url $repo_url 2>/dev/null || \
     kargo update credentials github-creds${i} \
     --project $project --git \
-    --username ${GITHUB_USER} --password ${GH_PAT} \
+    --username ${GITHUB_USER} --password ${GITHUB_PAT} \
     --repo-url $repo_url
     let i=i+1
 done
 
-if [ "$image_url" ~= "ghcr.io/akuity" ]; then
+if [[ "$image_url" =~ "ghcr.io/akuity" ]]; then
     echo -e "\t adding creds for image URL ${image_url}"
     kargo create credentials ghcr-creds \
     --project $project --image \
-    --username ${GITHUB_USER} --password ${GH_PAT} \
+    --username ${GITHUB_USER} --password ${GITHUB_PAT} \
     --repo-url $image_url 2>/dev/null || \
     kargo update credentials ghcr-creds \
     --project $project --image \
-    --username ${GITHUB_USER} --password ${GH_PAT} \
+    --username ${GITHUB_USER} --password ${GITHUB_PAT} \
     --repo-url $image_url
 else
     echo -e "\t skipping image creds for image URL ${image_url} because not ghcr.io/akuity"
@@ -47,7 +47,7 @@ echo -e "\tURL: $wh_url"
 curl -Ls \
         -X POST \
         -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${GH_PAT}" \
+        -H "Authorization: Bearer ${GITHUB_PAT}" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
         "https://api.github.com/repos/$GITHUB_ORG/sedemo-rollouts-app/hooks" \
         -d '{"name":"Kargo-Webhook","active":true,"events":["push"],"config":{"url":"'$wh_url'","content_type":"json","secret":"thisisverysecret"}}'
